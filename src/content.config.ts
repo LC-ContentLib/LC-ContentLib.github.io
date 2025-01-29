29e0ba7ab2973b8857c855d8fa2561c42e78e404
Hamunii <samuel.suni04@gmail.com>
2025-01-29 09:19:22 +0200

    update astro to 5.1.10 and starlight to 0.31.1

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,10 @@
 import { z, defineCollection } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({
+		loader: docsLoader(),
 		schema: docsSchema({
 			extend: z.object({
 				description: z.string(),
